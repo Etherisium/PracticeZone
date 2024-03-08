87b2e857d52d0e0df7caf8b2807e99c8b23f3d29
Etherisium <zarin.sumaiya@gmail.com>
2024-03-08 18:31:49 +0600

    first commit

---
+++ march5th2.js
@@ -0,0 +1,27 @@
+var numbers = [21,50,37,42,38,26,11,35];
+var evensum = 0;
+var oddsum=0;
+var oddcount = 0;
+
+
+for (vara =i; a < numbers.length; a++)
+{
+
+If (numbers[a-1]%2===0)
+
+{ evensum += numbers[a-1];
+evensum=evensum + numbers[a-1]; }
+
+
+else 
+
+{ 
+    oddsum += numbers[a-1];
+oddcount ++;
+
+}
+
+
+}
+
+var oddavg = oddsum/oddcount;
